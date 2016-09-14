835d788ae45abc1c28baa1b906e16098bfd6123b
zhou <liuzhouwork@gmail.com>
2016-09-14 00:27:56 -0500

    finished inclass-7

---
+++ inclass-7/inclass-fetch.js
@@ -0,0 +1,100 @@
+// Inclass Fetch Exercise
+// ======================
+//
+// Navigate to https://webdev-dummy.herokuapp.com/sample
+//
+// This endpoint returns a list of articles.  Your assignment is to
+// write a function countWords that uses fetch() to query the endpoint,
+// and return a map from the article id to the number of words in the
+// article's text.
+//
+// Also write two "helper" functions that call this initial function.
+//
+// If there are any exceptions then fetch() will throw an error.
+// Provide a "safe" version of the countWords function that always
+// returns a map, which will be empty in the case of errors.
+//
+// Finally, write a function that returns the article id with the
+// most number of words.
+//
+// Below I have provided you a template, you just need to fill in
+// the implementation.
+//
+// Navigate to mocha-inclass-fetch.html to see if your implementation
+// provides the expected results.
+//
+(function(exports) {
+
+    'use strict'
+
+    function countWords(url) {
+        // r is the http request response
+        return fetch(url).then(r => {
+                // parse the json
+                return r.json().then(json =>{
+                    //map store the result
+                    var map = {};
+                    var articles = json.articles
+                    for(var i=0; i < articles.length; i++){
+                        var count = articles[i].text.split(' ').length
+                        var id = articles[i]._id;
+                        map[id] = count
+                    }
+                    //return the result
+                    return map
+                })
+        })
+    }
+
+    function countWordsSafe(url) {
+        // r is the http request response
+        return fetch(url).then(r => {
+            if(r.ok){
+                // parse the json
+                return r.json().then(json =>{
+                    var map = {};
+                    var articles = json.articles
+                    for(var i=0; i < articles.length; i++){
+                        var count = articles[i].text.split(' ').length
+                        var id = articles[i]._id;
+                        map[id] = count
+                    }
+                    return map
+                })
+            } else {
+                throw new Error("Bad URL")
+            }
+        }).catch(e => {
+            // return an empty map
+            return {};
+        })
+    }
+
+    function getLargest(url) {
+        // r is the http request response
+        return fetch(url).then(r => {
+                // parse the json
+                return r.json().then(json =>{
+                    var max = 0;
+                    var maxID = "";
+                    var articles = json.articles
+                    for(var i=0; i < articles.length; i++){
+                        var count = articles[i].text.split(' ').length
+                        var id = articles[i]._id;
+                        if(count > max){
+                            max = count
+                            maxID = id
+                        }
+                    }
+                    // transfer the result into a string
+                    return maxID + ""
+                })
+        })
+    }
+
+    exports.inclass = {
+        author: "Zhou Liu",
+        countWords, countWordsSafe, getLargest
+    }
+
+})(this);
